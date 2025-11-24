613ec3910ad42dde18417e10357e8c75f6133909
Caio <caio.lima1991@icloud.com>
2025-11-24 14:49:18 -0300

    fix: resolve problema do jdtls attaching em um unico arquivo

--- .config/nvim/lua/plugins/nvim_jdtls.lua
+++ .config/nvim/lua/plugins/nvim_jdtls.lua
@@ -4,7 +4,23 @@ return {
     ft = "java",
     config = function()
       local get_jdtls_config = require("config.lsp.jdtls")
-      require("jdtls").start_or_attach(get_jdtls_config())
+
+      local function jdtls_autostart()
+        for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+          if client.name == "jdtls" then
+            return
+          end
+        end
+        require("jdtls").start_or_attach(get_jdtls_config())
+      end
+
+      vim.api.nvim_create_augroup("JDTLS_LSP_START", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = "JDTLS_LSP_START",
+        pattern = "java",
+        callback = jdtls_autostart,
+        desc = "Start nvim-jdtls for Java files",
+      })
     end,
   },
 
