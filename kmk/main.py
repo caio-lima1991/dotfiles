5cc1acd2fab91010fd8244b907faa33e8fef2143
Caio <caio.lima1991@icloud.com>
2026-01-04 18:57:56 -0300

    feat: config abnt2 teclado

--- kmk/main.py
+++ kmk/main.py
@@ -23,23 +23,21 @@ keyboard.coord_mapping = [
     ]
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-# ABNT2 layout
 keyboard.keymap = [
     [
         KC.ESC,     KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5,      KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.BSPC,
-        KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,       KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.LBRC,
-        KC.MO(1),   KC.A,       KC.S,       KC.D,       KC.F,       KC.G,       KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT,
+        KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,       KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.DELETE,
+        KC.LCTRL,   KC.A,       KC.S,       KC.D,       KC.F,       KC.G,       KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT,
         KC.LSFT,    KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,       KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.RSFT,
-                                KC.LGUI,    KC.LCTL,    KC.ENTER,   KC.HOME,    KC.END,     KC.MO(1),    KC.SPACE,   KC.RALT
+                                KC.LGUI,    KC.ENTER,   KC.MO(1),   KC.HOME,    KC.END,     KC.MO(1),   KC.SPACE,   KC.RALT
     ],
     [
         KC.F12,     KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5,      KC.F6,      KC.F7,      KC.F8,      KC.F9,      KC.F10,         KC.F11,
-        KC.GRV,     KC.MPRV,    KC.MNXT,    KC.MPLY,    KC.NO,      KC.NO,      KC.MINS,    KC.EQL,     KC.RBRC,    KC.BSLS,    KC.NONUS_BSLASH,KC.PGUP,
-        KC.TRNS,    KC.MUTE,    KC.VOLD,    KC.VOLU,    KC.NO,      KC.NO,      KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   KC.NO,          KC.PGDN,
-        KC.LSFT,    KC.CAPS,    KC.X,       KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,          KC.RSFT,
+        KC.GRV,     KC.MPRV,    KC.MNXT,    KC.MPLY,    KC.NO,      KC.NO,      KC.MINS,    KC.EQL,     KC.RBRC,    KC.BSLS,    KC.NONUS_BSLASH,KC.LBRC,
+        KC.LCTRL,   KC.MUTE,    KC.VOLD,    KC.VOLU,    KC.PGDOWN,  KC.PGUP,    KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   KC.NO,          KC.NO,
+        KC.LSFT,    KC.CAPS,    KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,          KC.RSFT,
                                 KC.NO,      KC.LCTL,    KC.NO,      KC.NO,      KC.NO,      KC.TRNS,    KC.NO,      KC.NO
-    ]
-]
+    ]]
 
 if __name__ == '__main__':
     keyboard.go()
